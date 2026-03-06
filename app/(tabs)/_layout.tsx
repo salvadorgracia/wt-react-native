bfbf909e0037bf6c172c4a2525fd417befefd3b0
Salvador Gracia <midnightzada@gmail.com>
2026-03-05 20:01:33 -0800

    feat: implement custom tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,25 +1,141 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { images } from '@/constants/images'
+import {
+  Image,
+  ImageBackground,
+  Text,
+  View,
+  Pressable,
+} from 'react-native'
+import { icons } from '@/constants/icons'
+
+const TabIcon = ({ focused, icon, title }: any) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 
+          mt-4 justify-center items-center rounded-full overflow-hidden"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold">
+          {title}
+        </Text>
+      </ImageBackground>
+    )
+  } else {
+    return (
+      <View className="size-full justify-center items-center mt-4 rounded-full">
+        <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+      </View>
+    )
+  }
+}
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: '100%',
+          height: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        tabBarStyle: {
+          backgroundColor: '#0F0D23',
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: 'absolute',
+          overflow: 'hidden',
+          borderWidth: 1,
+          borderColor: '#0F0D23',
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
-        options={{ title: 'Home', headerShown: false }}
+        options={{
+          title: 'Home',
+          headerShown: false,
+          tabBarButton: (props) => (
+            <Pressable
+              {...(props as any)}
+              hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}
+            />
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={icons.home}
+              title="Home"
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="search"
-        options={{ title: 'Search', headerShown: false }}
+        options={{
+          title: 'Search',
+          headerShown: false,
+          tabBarButton: (props) => (
+            <Pressable
+              {...(props as any)}
+              hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}
+            />
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={icons.search}
+              title="Search"
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="saved"
-        options={{ title: 'Saved', headerShown: false }}
+        options={{
+          title: 'Saved',
+          headerShown: false,
+          tabBarButton: (props) => (
+            <Pressable
+              {...(props as any)}
+              hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}
+            />
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={icons.save}
+              title="Saved"
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="profile"
-        options={{ title: 'Profile', headerShown: false }}
+        options={{
+          title: 'Profile',
+          headerShown: false,
+          tabBarButton: (props) => (
+            <Pressable
+              {...(props as any)}
+              hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}
+            />
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={icons.person}
+              title="Profile"
+            />
+          ),
+        }}
       />
     </Tabs>
   )
