d9a479be5148f77c84a8c8191fc74dde92b89f6a
Salvador Gracia <midnightzada@gmail.com>
2026-03-06 10:42:05 -0800

    chore: add .nvmrc with current version

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -18,7 +18,6 @@ const TabIcon = ({ focused, icon, title }: any) => {
         source={images.highlight}
         className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 
           mt-4 justify-center items-center rounded-full overflow-hidden"
-        style={d.blue}
       >
         <Image source={icon} tintColor="#151312" className="size-5" />
         <Text className="text-secondary text-base font-semibold">
@@ -30,7 +29,6 @@ const TabIcon = ({ focused, icon, title }: any) => {
     return (
       <View
         className="size-full justify-center items-center mt-4 rounded-full"
-        style={d.green}
       >
         <Image source={icon} tintColor="#A8B5DB" className="size-5" />
       </View>
